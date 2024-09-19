25bab9122d866eff664b36148315dcda18ad0158
Greg Denyes <greg.denyes@gmail.com>
2024-09-19 13:24:23 -0400

    python build finally works... sharing this up so others can use it.

---
+++ changelog.md
@@ -0,0 +1,7 @@
+# Changelog for GLogger
+
+
+
+<!--  NEW CHANGES   /-->
+
+# **GLogger created & maintained by:** [me](http://here.com)
--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+import os
 import json
-from setuptools import setup
+from setuptools import setup, find_packages
 
+data_files = [('glogger', ['glogger/glog.json'])]
+
+# with open(os.path.join(os.path.dirname(__file__), "glogger", "glog.json"), "r", encoding="utf-8") as f:
 with open("glogger/glog.json", "r", encoding="utf-8") as f:
     data = json.loads(f.read())
 
 setup(
     name=data['app_title'],
     version=".".join(str(x) for x in data['version_number']),
-    packages=['glogger'],
+    # packages=['glogger'],
     # packages=['glogger', 'jinja2'],
+    packages=find_packages(),
+    data_files=data_files,
     dependency_links=['jinja2'],
     entry_points={
         'console_scripts': [
